925942cbde224be673554a833d5660b0113374f7
Krzysiek <krzysztof.tyszkiewicz.kt@gmail.com>
2021-07-12 23:32:13 +0200

    aktualizacja

---
+++ SFML_sandbox/SFML_sandbox/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <time.h>
+#include <sstream>
+using namespace std;
+using namespace sf;
+
+int howmany(int x, int* map);
+bool check(int x, int* map);
+void prepare(int* map);
